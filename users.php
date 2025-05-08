c6bbec0b4939c261d8897dca84f8f3b51f9af615
Asmaa-mi <mikazoldyck568@gmail.com>
2025-05-08 14:35:47 +0300

    commit-users

--- users.php
+++ users.php
@@ -1,13 +1,38 @@
 <?php
+session_start();
+if (!isset($_SESSION['id']) || $_SESSION['role'] != 'admin') {
+    die("Failed!!!");
+}
+$conn = mysqli_connect("localhost", "root", "", "mystore");
+$users = mysqli_query($conn, "SELECT * FROM users");
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Users</title>
+    <title>Manage Users</title>
 </head>
+
 <body>
-    <h1>Users</h1>
+    <h1>User Management</h1>
+    <table border="1">
+        <tr>
+            <th>Id</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Role</th>
+        </tr>
+        <?php while ($usee = mysqli_fetch_assoc($users)): ?>
+            <tr>
+                <td><?= $usee['id'] ?></td>
+                <td><?= htmlspecialchars($usee['username']) ?></td>
+                <td><?= htmlspecialchars($usee['email']) ?></td>
+                <td><?= htmlspecialchars($usee['age']) ?></td>
+                <td><?= $usee['role'] ?></td>
+            </tr>
+        <?php endwhile; ?>
+    </table>
 </body>
+
 </html>
