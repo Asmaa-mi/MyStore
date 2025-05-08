43544b0c89f5da158941789d68fbfcf3d24af4ba
Asmaa-mi <mikazoldyck568@gmail.com>
2025-05-08 14:38:51 +0300

    logout-commit

--- logout.php
+++ logout.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logout</title>
-</head>
-<body>
-    <h1>Logout</h1>
-</body>
-</html>
+<?php
+session_start();
+session_unset();
+session_destroy();
+header("Location: index.php");
+exit;
+?>
