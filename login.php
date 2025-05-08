5909da9208778a91f9105dfdc0bdca02ebc17c7a
Asmaa-mi <mikazoldyck568@gmail.com>
2025-05-08 14:39:16 +0300

    login-commit

--- login.php
+++ login.php
@@ -1,40 +1,64 @@
 <?php
-$email="";
-if($_SERVER['REQUEST_METHOD']== "POST") {
+session_start();
+$errors = [];
+$email = "";
+
+$conn = mysqli_connect("localhost", "root", "", "mystore");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $email = trim($_POST["email"]);
     $password = trim($_POST["password"]);
-    if(empty($email)) {
+    if (empty($email)) {
         $errors['email'] = "Email is required";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "Invalid email";
+
     }
-    elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors['email'] = "Invalid email format"; 
-    }
-    if(empty($password)) {
+
+    if (empty($password)) {
         $errors['password'] = "Password is required";
+    } elseif (strlen($password) < 6) {
+        $errors['password'] = "Password must be at least 6 characters long!";
     }
-    elseif(strlen($password) < 6) {
-        $errors['password'] = "Password must be at least 6 characters long";
+
+    if (empty($errors)) {
+        $result = mysqli_query($conn, "SELECT * FROM users WHERE email='$email'");
+        $user = mysqli_fetch_assoc($result);
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['role'] = $user['role'];
+            header("Location: index.php");
+            exit;
+        } else {
+            $errors['general'] = "Invalid login";
+        }
     }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
+
 <body>
     <h1>Login</h1>
     <form method="POST">
-    <div>
+        <div>
             <label for="email">Email</label>
-            <input type="text" name="email" id="email" value="<?= htmlspecialchars($email);?>">
+            <input type="text" name="email" id="email" value="<?= htmlspecialchars($email); ?>">
         </div>
         <div>
-            <?php if(isset($errors['email'])) : ?>
-                <p style="color : red;"><?= $errors['email']?></p>
-                <?php endif;  ?>
+            <?php if (isset($errors['email'])): ?>
+                <p style="color : red;"><?= $errors['email'] ?></p>
+            <?php endif; ?>
         </div>
         <br>
         <div>
@@ -42,16 +66,19 @@ if($_SERVER['REQUEST_METHOD']== "POST") {
             <input type="password" name="password" id="password">
         </div>
         <div>
-            <?php if(isset($errors['password'])) : ?>
-                <p style="color : red;"><?= $errors['password']?></p>
-                <?php endif;  ?>
+            <?php if (isset($errors['password'])): ?>
+                <p style="color : red;"><?= $errors['password'] ?></p>
+            <?php endif; ?>
         </div>
         <br>
         <div>
             <button type="submit"><a href="index.php">Submit</a></button>
         </div>
         <br>
+        <h3><a href="products.php">Your Products</a></h3>
+        <h3 style="color: blue;">Do not have Account?</h3>
         <a href="register.php">Create Account</a>
     </form>
 </body>
+
 </html>
