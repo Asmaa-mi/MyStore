8aa422f05773802615d9e9da7ccb1068ad163ad1
Asmaa-mi <mikazoldyck568@gmail.com>
2025-05-08 14:37:30 +0300

    product-commit

--- products.php
+++ products.php
@@ -1,11 +1,33 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "root", "", "mystore");
+$conn = mysqli_connect("localhost", "root", "", "mystore");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$products = mysqli_query($conn, "SELECT * FROM products");
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Products</title>
+    <title>Manage Products</title>
 </head>
 <body>
-    <h1>Products</h1>
+    <h1>Product Management</h1>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+        </tr>
+        <?php while ($p = mysqli_fetch_assoc($products)): ?>
+            <tr>
+                <td><?= $p['id'] ?></td>
+                <td><?= htmlspecialchars($p['product_name']) ?></td>
+                <td><?= $p['price'] ?></td>
+            </tr>
+        <?php endwhile; ?>
+    </table>
 </body>
+
 </html>
